d2acc28d67bb89dd99abf2f5ea019ab5b7d05d10
PagarePramodini <pramodini@psoftsolutions.in>
2026-02-02 15:01:45 +0530

    get bank details method

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,11 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://anygoUser:BOJkZdLjG5Q5Z6pR@anygo.qwztb8w.mongodb.net/Anygo'),OwnerModule, CustomersModule, 
+  //imports: [ MongooseModule.forRoot('mongodb+srv://anygoUser:BOJkZdLjG5Q5Z6pR@anygo.qwztb8w.mongodb.net/Anygo'),OwnerModule, CustomersModule, 
+  //DriversModule, AuthModule, CommonModule,MasterModule,PaymentModule,
+  //ConfigModule.forRoot({ isGlobal: true,}),GatewaysModule,ScheduleModule.forRoot()],
+
+  imports: [ MongooseModule.forRoot('mongodb://localhost:27017/Porter-App'),OwnerModule, CustomersModule, 
   DriversModule, AuthModule, CommonModule,MasterModule,PaymentModule,
   ConfigModule.forRoot({ isGlobal: true,}),GatewaysModule,ScheduleModule.forRoot()],
   controllers: [AppController],
